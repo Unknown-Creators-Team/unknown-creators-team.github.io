b156bf03de34db98e159eb87d1b63b4d3d345c20
Nano191225 <93137582+191225@users.noreply.github.com>
2023-07-22 11:26:54 +0900

    update 404.html, users.json, 404.js and scripts/team/index.js

--- scripts/404.js
+++ scripts/404.js
@@ -1,3 +1,4 @@
 $(() => {
-    $("#url").text(window.location.pathname);
+    const path = new Map(window.location.search.split('?').slice(1).join('?').split('&').map((v) => v.split('=')));
+    $("#url").text(path.get("url"));
 })
